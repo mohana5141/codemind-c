2b9b607897ee4dfe0726cb0c869fcce33f7bd46d
mohana5141 <147473112+mohana5141@users.noreply.github.com>
2024-03-01 06:08:26 +0530

    #include <stdio.h> int main() {    int n,i,min=0;    scanf("%d",&n);    int arr[n];    for(i=0;i<n;i++){        scanf("%d",&arr[i]);    }      for(i=0;i<n;i++){         if(arr[i]%2!=0){             min=i;         }
        }
           printf("%d",arr[min]); 
    }

---
+++ Last_odd_number_in_an_Array.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+   int n,i,min=0;
+   scanf("%d",&n);
+   int arr[n];
+   for(i=0;i<n;i++){
+       scanf("%d",&arr[i]);
+   }
+     for(i=0;i<n;i++){
+        if(arr[i]%2!=0){
+            min=i;
+        }
+        
+    }
+       printf("%d",arr[min]); 
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
