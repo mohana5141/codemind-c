7ee6e1702b9003be2fd6ce3bfc4614a693778583
mohana5141 <147473112+mohana5141@users.noreply.github.com>
2024-03-06 11:18:58 +0530

    #include <stdio.h> int main() {   char str[101];   scanf("%[^ ]%c",&str);   int i,cnt=0;   for(i=0;str[i]!=NULL;i++){       cnt++;   }    printf("%d",cnt);
    }

---
+++ Characters_in_a_String.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+  char str[101];
+  scanf("%[^
+]%c",&str);
+  int i,cnt=0;
+  for(i=0;str[i]!=NULL;i++){
+      cnt++;
+  }
+   printf("%d",cnt); 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
