6ba1171c990a694682f13a8ade3b568a7df05260
mohana5141 <147473112+mohana5141@users.noreply.github.com>
2024-03-07 22:57:51 +0530

    #include <stdio.h> int main(){     int x,y,z;     scanf("%d %d %d",&x,&y,&z);     int c=(y*z);     if(x%c==0){         printf("%d",(x/c));     }     else{         printf("%d",(x/c)+1);     }
    }

---
+++ Bags_and_Candies.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+    int x,y,z;
+    scanf("%d %d %d",&x,&y,&z);
+    int c=(y*z);
+    if(x%c==0){
+        printf("%d",(x/c));
+    }
+    else{
+        printf("%d",(x/c)+1);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
