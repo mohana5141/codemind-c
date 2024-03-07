7d55fcf658a3be64786dbfd65c83b61014b79825
mohana5141 <147473112+mohana5141@users.noreply.github.com>
2024-03-07 23:16:38 +0530

    #include <stdio.h> int main() {    int a,b;    scanf("%d %d",&a,&b);    int x=21-(a+b);    if(x>10){
           printf("-1");
               
           
       }
       else{ 
           printf("%d",x);
       } 
        
        
        
        
        
        
        
        
        
        
        
        
        
    }

---
+++ Blackjack.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+   int a,b;
+   scanf("%d %d",&a,&b);
+   int x=21-(a+b);
+   if(x>10){
+       
+       printf("-1");
+           
+       
+   }
+   else{ 
+       printf("%d",x);
+   } 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
