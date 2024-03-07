59e5fd6fafb2c5b758b96835c3d2598c407f9a8b
mohana5141 <147473112+mohana5141@users.noreply.github.com>
2024-03-07 23:26:49 +0530

    #include <stdio.h> int main() {   int n;   scanf("%d",&n);   if(n%5==0){       printf("%d",(n/10)+n%10/5);   }
      else {
          printf("-1");
      }  
        
        
        
        
        
        
        
        
    }

---
+++ Kohli_and_Coins.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+  int n;
+  scanf("%d",&n);
+  if(n%5==0){
+      printf("%d",(n/10)+n%10/5);
+  }
+   
+  else {
+      printf("-1");
+  }  
+    
+    
+    
+    
+    
+    
+    
+    
+}
