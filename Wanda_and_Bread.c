c535d7a8e970d8b38b53553d4b94c4dca7a83cbb
mohana5141 <147473112+mohana5141@users.noreply.github.com>
2024-03-08 21:55:01 +0530

    #include <stdio.h> int main() {   int a,b,c;   scanf("%d %d %d",&a,&b,&c);   int d=b*c;   if(d>=a){       printf("YES");   }   else if(d<a){       printf("NO");   }
    }

---
+++ Wanda_and_Bread.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+  int a,b,c;
+  scanf("%d %d %d",&a,&b,&c);
+  int d=b*c;
+  if(d>=a){
+      printf("YES");
+  }
+  else if(d<a){
+      printf("NO");
+  }  
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
